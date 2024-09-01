c54ce29118278cb12f163ffe3026d320ecf6a958
Kooreis <kooreis.official@gmail.com>
2024-09-01 05:26:06 -0500

    This line of the script calls the SolveLightsOut function to calculate the solution for the given grid.

--- solution.cs
+++ solution.cs
@@ -1,17 +1,1 @@
-using System;
-using System.Linq;
-
-class Program
-{
-    static void Main()
-    {
-        int[,] grid = new int[5, 5];
-        for (int i = 0; i < 5; i++)
-        {
-            Console.WriteLine("Enter row " + (i + 1) + " (5 digits 0 or 1, separated by space):");
-            string[] input = Console.ReadLine().Split(' ');
-            for (int j = 0; j < 5; j++)
-            {
-                grid[i, j] = int.Parse(input[j]);
-            }
-        }
+int[,] solution = SolveLightsOut(grid);
